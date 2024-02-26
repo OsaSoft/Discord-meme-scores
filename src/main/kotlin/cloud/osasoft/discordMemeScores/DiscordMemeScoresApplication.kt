c207551801fba7113176532954a123d5f16b61c9
Oscar Hernandez <OsaSofto@gmail.com>
2024-02-26 22:23:06 +0100

    basic spring project

---
+++ src/main/kotlin/cloud/osasoft/discordMemeScores/DiscordMemeScoresApplication.kt
@@ -0,0 +1,11 @@
+package cloud.osasoft.discordMemeScores
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiscordMemeScoresApplication
+
+fun main(args: Array<String>) {
+	runApplication<DiscordMemeScoresApplication>(*args)
+}
---
+++ src/test/kotlin/cloud/osasoft/discordMemeScores/DiscordMemeScoresApplicationTests.kt
@@ -0,0 +1,13 @@
+package cloud.osasoft.discordMemeScores
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiscordMemeScoresApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
