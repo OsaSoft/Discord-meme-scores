a86da7991089220889f85fd72fda1ca78f5667eb
Oscar Hernandez <OsaSofto@gmail.com>
2024-03-02 14:30:10 +0100

    working stats command

--- src/main/kotlin/cloud/osasoft/discordMemeScores/DiscordMemeScoresApplication.kt
+++ src/main/kotlin/cloud/osasoft/discordMemeScores/DiscordMemeScoresApplication.kt
@@ -1,8 +1,14 @@
 package cloud.osasoft.discordMemeScores
 
 import cloud.osasoft.discordMemeScores.config.DiscordProperties
+import dev.kord.common.entity.Permission
+import dev.kord.common.entity.Permissions
 import me.jakejmattson.discordkt.dsl.bot
 
 fun main() {
-    bot(DiscordProperties.getToken()) {}
+    bot(DiscordProperties.getToken()) {
+        configure {
+            defaultPermissions = Permissions(Permission.ReadMessageHistory, Permission.SendMessages)
+        }
+    }
 }
--- src/main/kotlin/cloud/osasoft/discordMemeScores/commands/MemeCmd.kt
+++ src/main/kotlin/cloud/osasoft/discordMemeScores/commands/MemeCmd.kt
@@ -1,21 +1,49 @@
 package cloud.osasoft.discordMemeScores.commands
 
-import kotlinx.coroutines.flow.count
 import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.takeWhile
+import kotlinx.coroutines.flow.toList
+import kotlinx.datetime.toKotlinInstant
 import me.jakejmattson.discordkt.commands.commands
+import me.jakejmattson.discordkt.util.author
 import me.jakejmattson.discordkt.util.isImagePost
+import java.time.Instant
+import kotlin.time.Duration.Companion.days
+import kotlin.time.toJavaDuration
 
 fun memeListener() = commands("Memes") {
-    slash("countMemes") {
+    slash("memeStats") {
         execute {
-            val count = this.channel.messages
-                .filter { it.author?.id == this.author.id }
+            val cutOffDuration = 7.days
+            val cutOffDate = Instant.now() - cutOffDuration.toJavaDuration()
+
+            val author = this.author
+            val memes = this.channel.messages
+                .takeWhile { it.timestamp >= cutOffDate.toKotlinInstant() }
+                .filter { it.author?.id == author.id }
                 .filter { it.isImagePost() }
-                .count()
+                .toList()
+
+            val reactions: Map<String, Int> = buildMap {
+                memes.flatMap { it.reactions }.forEach { reaction ->
+                    val name = reaction.emoji.name
+                    val currentCount = getOrDefault(name, 0)
+                    put(name, currentCount + reaction.count)
+                }
+            }
 
             this.respondPublic {
+                this.author(author)
                 title = "Meme Count"
-                description = "${this.author} has posted $count memes."
+                description = buildString {
+                    append("${author.username} has posted ${memes.size} memes in the past $cutOffDuration")
+                    if (memes.isNotEmpty()) {
+                        appendLine()
+                        append("Gathering the following reactions:")
+                        appendLine()
+                        reactions.forEach { (emoji, count) -> appendLine("$emoji: $count") }
+                    }
+                }
             }
         }
     }
